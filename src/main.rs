68b231a6827b113f56d9a075d34783345c29f3e5
Paweł Kopel <pawel.kopel2@gmail.com>
2021-02-14 21:59:35 +0100

    add option to play again, improve cli

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
+use std::io::{self,Write};
 
 fn user_guess() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Guess the number between 1 ad 100:");
+    print!("Guess a number between 1 ad 100.");
 
     loop {
+        print!("Your guess: ");
+        io::stdout().flush().unwrap();
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -15,7 +17,10 @@ fn user_guess() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please enter an integer");
+                continue;
+            },
         };
 
         match guess.cmp(&secret_number) {
@@ -30,14 +35,16 @@ fn user_guess() {
 }
 
 fn computer_guess() {
-    println!("Think a number between 1 and 100 and press enter.");
+    print!("Think a number between 1 and 100 and press enter.");
+    io::stdout().flush().unwrap();
     io::stdin().read_line(&mut String::new()).expect("error");
     let mut high = 100;
     let mut low = 1;
     let mut mid: usize;
     loop {
         mid = (high + low) / 2;
-        println!("Is your number equal {}?\n('yes', 'less' or 'more')", mid);
+        print!("Is your number equal {}? [yes|less|more]: ", mid);
+        io::stdout().flush().unwrap();
         let mut response = String::new();
 
         io::stdin()
@@ -47,30 +54,38 @@ fn computer_guess() {
             "less" => high = mid,
             "more" => low = mid,
             "yes" => break,
-            _ => println!("Please respond with 'yes', 'less' or 'more'"),
+            _ => println!("Please respond with yes, less or more"),
         }
     }
     println!("Your number is {}", mid);
 }
 
-fn main() {
-    println!("Who should guess?\n1: You\n2: computer");
+fn main() { 
     loop {
+        print!("Who should guess?\n1: You\n2: computer\nYour choice: ");
         let mut choice = String::new();
+        io::stdout().flush().unwrap();
         io::stdin()
             .read_line(&mut choice)
             .expect("error while reading input");
 
         match choice.trim().parse() {
-            Ok(1) => {
-                user_guess();
-                break;
-            }
-            Ok(2) => {
-                computer_guess();
-                break;
-            }
+            Ok(1) => user_guess(),
+            Ok(2) => computer_guess(),
             _ => continue,
         };
-    }
+
+        choice = String::new();
+
+        print!("Try again? [yes|no]: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("error while reading input");
+
+        match choice.trim() {
+            "yes" => continue,
+            _ => break,
+        };
+    };
 }
